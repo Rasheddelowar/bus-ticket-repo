c7817fd53cdbc364c3bcd8dfb609c5772572e86f
delowar hossain <hossaind633@gmail.com>
2024-02-21 16:12:51 +0600

    bus ticket

---
+++ bus_ticket.js
@@ -0,0 +1,6 @@
+const allbtn=document.getElementsByClassName("btn");
+for(const btn of allbtn){
+btn.addEventListener("click", function (event){
+   const name = event.target.parentNodes[1.innerText]
+})
+}
